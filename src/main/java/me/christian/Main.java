82c9dc0587f3dd8db7bf6220cb0c549b5ea41e65
Christian <sweatchristian03@gmail.com>
2022-10-03 15:18:47 -0400

    Added BinarySearch.find()

--- src/main/java/me/christian/Main.java
+++ src/main/java/me/christian/Main.java
@@ -1,18 +1,34 @@
 package me.christian;
 
-import java.util.Scanner;
+import me.christian.binary_search.BinarySearch;
+
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+        final BinarySearch binarySearch = new BinarySearch();
 
-        while(scanner.hasNext()) {
+        // Run 10 times to give user a few runs to see the difference.
+        for (int i = 0; i < 10; i++) {
             System.out.println("Please enter an array size: ");
             int num = scanner.nextInt();
+            int[] arrayToProcess = generateRandomSortedArray(num);
+            System.out.println("Here is your array of numbers:\n"+Arrays.toString(arrayToProcess));
+            System.out.println("Please pick a number from this list.. or not");
+            int target = scanner.nextInt();
 
-
+            long startMilli = System.currentTimeMillis();
+            System.out.printf("%s is located at index %s.", target, binarySearch.find(target, arrayToProcess));
+            System.out.printf("Total milliseconds taken: %sms", System.currentTimeMillis() - startMilli);
         }
+    }
 
-
+    private static int[] generateRandomSortedArray(int size) {
+        int[] listOfInts = new int[size+1];
+        for (int i = 0; i < size; i++)
+            listOfInts[i] = new Random().nextInt(size * 2);
+        Arrays.sort(listOfInts);
+        return listOfInts;
     }
 }
--- src/main/java/me/christian/binary_search/BinarySearch.java
+++ src/main/java/me/christian/binary_search/BinarySearch.java
@@ -1,4 +1,21 @@
 package me.christian.binary_search;
 
 public class BinarySearch {
+
+    public int find(int target, int[] list) {
+        int left = 0, right = list.length - 1;
+        while (left <= right) {
+            int middle = (left + right)/2;
+
+            if (target == list[middle]) {
+                return middle;
+            } else if (target > list[middle]) {
+                left = middle + 1;
+            } else if (target < list[middle]) {
+                right = middle - 1;
+            }
+        }
+        return -1;
+    }
+
 }
